48b232cd58e86221a8fc14d58bb1a5bce57484f7
Pat Schloss <pschloss@umich.edu>
2015-07-09 22:42:14 -0400

    Generalize code

--- invoice_generator.R
+++ invoice_generator.R
@@ -26,64 +26,77 @@ spread_sheet <- read.xlsx("purchase_records.xlsx", sheet=1, startRow=1, colNames
 
 patrons <- levels(factor(spread_sheet$Name))
 
-make_email <- function(surname){
-	breslin <- spread_sheet[spread_sheet$Name==surname,]
-	patron <- breslin[1,2]
-	dates <- breslin[,1]
-	returns <- breslin[,3]
-	breslin <- breslin[,-c(1,2,3)]
+make_email <- function(full_name, password){
+	surname <- gsub(".* (.*)", "\\1", full_name)
+	firstname <- gsub("(.*) .*", "\\1", full_name)
 
-	n_entries <- sum(!is.na(breslin[,-ncol(breslin)]))
+	customer <- spread_sheet[spread_sheet$Name==surname,]
+	patron <- customer[1,2]
+	dates <- customer[,1]
+	returns <- customer[,3]
+	customer <- customer[,-c(1,2,3)]
+
+	n_entries <- sum(!is.na(customer[,-ncol(customer)]))
 	invoice_table <- data.frame(Date=rep("xxx", n_entries),
 								Items=rep("xxx", n_entries),
 								Quantity=rep(0, n_entries),
 								Price=rep(0, n_entries),
 								Total=rep(0, n_entries),
-								stringsAsFactors=FALSE
-								)
+								stringsAsFactors=FALSE )
 
 	counter <- 1
-	for(visit in 1:nrow(breslin)){
+	for(visit in 1:nrow(customer)){
 		for(category in names(price)){
-			if(!is.na(breslin[visit,category])){
+			if(!is.na(customer[visit,category])){
 
 				invoice_table[counter, "Date"] <- as.character(dates[visit])
 
 				if(category != "OtherCategory"){
 					invoice_table[counter, "Items"] <- category
-					invoice_table[counter, "Quantity"] <- breslin[visit,category]
+					invoice_table[counter, "Quantity"] <- customer[visit,category]
 					invoice_table[counter, "Price"] <- price[category]
-					invoice_table[counter, "Total"] <- price[category] * breslin[visit,category]
+					invoice_table[counter, "Total"] <- price[category] * customer[visit,category]
 				} else {
-					invoice_table[counter, "Items"] <- breslin[visit,category]
+					invoice_table[counter, "Items"] <- customer[visit,category]
 					invoice_table[counter, "Quantity"] <- 1
-					invoice_table[counter, "Price"] <- breslin[visit,"OtherPrice"]
-					invoice_table[counter, "Total"] <- breslin[visit,"OtherPrice"]
+					invoice_table[counter, "Price"] <- customer[visit,"OtherPrice"]
+					invoice_table[counter, "Total"] <- customer[visit,"OtherPrice"]
 				}
 				counter <- counter + 1
 			}
-
 		}
 	}
 	invoice_table$Price <- format(invoice_table$Price, nsmall=2L)
 	invoice_table$Total <- format(invoice_table$Total, nsmall=2L)
 
-	pretty_table <- kable(invoice_table, format="pandoc", align=c('l', 'l', 'c', 'r', 'r'))
+	pretty_table <- kable(invoice_table, align=c('l', 'l', 'c', 'r', 'r'))
 	grand_total <- sum(as.numeric(invoice_table$Total))
 
-	text <- paste0("Your total for the month of ", months(dates[1]), " is $", format(grand_total, nsmall=2L), ". The details of what items you obtained from Eight Kids Farm are in the table below. You can pay by cash or check the next time you visit. If you have questions about your bill, please email me at sjschloss@gmail.com or call (734)424-0331. We appreciate your support of our farm as we strive to provide our community with healthy food while raising our family.")
+	text <- paste0("Dear ", firstname, ",<br><br>We appreciate your support of our farm as we strive to provide our community with healthy food while raising our family. Your total for the month of ", months(dates[1]), " is $", format(grand_total, nsmall=2L), ". The details of what items you obtained from Eight Kids Farm are in the table below. You can pay by cash or check the next time you visit. If you have questions about your bill, please email me at sjschloss@gmail.com or call (413)548-4444.<br><br>Sincerely,<br>Sarah Schloss<br><br>")
 
-	body <- paste(c(text, "", "", pretty_table), collapse="\n")
+
+	write(paste(c(text, "", "", pretty_table), collapse="\n"), "email.Rmd")
+	knit2html("email.Rmd", options = "")
 
 	send.mail(from = "sjschloss@gmail.com",
-          to = c("pdschloss@gmail.com"),
-          subject = "Subject of the email",
-          body = body,
-          smtp = list(host.name = "aspmx.l.google.com", port = 25),
-          authenticate = FALSE,
-		  encoding = "us-ascii",
-          send = TRUE)
+		to = c("pdschloss@gmail.com"),
+		subject = "June bill from Eight Kids Farm",
+		body = "email.html",
+		html = TRUE,
+		inline = TRUE,
+		smtp = list(host.name = "smtp.gmail.com", port = 465, user.name = "sjschloss@gmail.com", passwd = password, ssl = TRUE),
+		authenticate = TRUE,
+		send = TRUE)
 
+	file.remove(c("email.Rmd", "email.md", "email.html"))
 }
 
-sapply(patrons, make_email)
+print("Please enter your password: ")
+password <- readLines(n=1)
+patrons <- c("Mike Breslin")
+sapply(patrons, make_email, password)
+
+
+# need to automate date in subject line
+# need to pull out date's data from spreadsheet
+# need to count milk jars
